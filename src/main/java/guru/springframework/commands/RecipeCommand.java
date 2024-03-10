91d5e169e8dccd64223e5fe8a3dba07f52e4ad47
John <john.refaat88@gmail.com>
2024-03-11 01:33:32 +0200

    Adding Validation

--- src/main/java/guru/springframework/commands/RecipeCommand.java
+++ src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -5,7 +5,13 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,13 +25,31 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min=3, max=255)
     private String description;
+
+    @Min(2)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(2)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(2)
+    @Max(999)
     private Integer servings;
+
     private String source;
+
+    @URL
     private String url;
+
+    @NotBlank
     private String directions;
+
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Difficulty difficulty;
     private NotesCommand notes;
